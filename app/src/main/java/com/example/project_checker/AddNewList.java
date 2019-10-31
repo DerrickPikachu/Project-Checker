4e599e694e0ffba013b7f0681d811653470675f1
pikaChin <derrfgh4563@gmail.com>
2019-10-31 15:14:34 +0800

    AddNewList can go back

--- app/src/main/java/com/example/project_checker/AddNewList.java
+++ app/src/main/java/com/example/project_checker/AddNewList.java
@@ -3,12 +3,20 @@ package com.example.project_checker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class AddNewList extends AppCompatActivity {
+public class AddNewList extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_list);
+
+        findViewById(R.id.saveBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
     }
 }
