b96b5f38659c632d75c0b92be238945b98b860aa
pikaChin <derrfgh4563@gmail.com>
2019-10-31 15:10:44 +0800

    able to call AddNewList class

--- app/src/main/java/com/example/project_checker/MainActivity.java
+++ app/src/main/java/com/example/project_checker/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.project_checker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.addListBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, AddNewList.class));
     }
 }
